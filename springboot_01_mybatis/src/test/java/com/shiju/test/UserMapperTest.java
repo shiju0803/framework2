d13642f7548bbd3279b79f081a5e88738308e427
shiju1283 <shiju1283@163.com>
2021-06-17 15:55:49 +0800

    2021/06/17 spring整合mybatis

--- springboot_01_mybatis/src/test/java/com/shiju/test/UserMapperTest.java
+++ springboot_01_mybatis/src/test/java/com/shiju/test/UserMapperTest.java
@@ -1,10 +1,15 @@
 package com.shiju.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.shiju.domain.User;
 import com.shiju.mapper.UserMapper;
 import com.shiju.mapper.UserXmlMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 /**
  * @author shiju
@@ -24,9 +29,15 @@ public class UserMapperTest {
         userMapper.findAll().forEach(s -> System.out.println(s));
     }
 
+    /**
+     * 测试分页插件
+     */
     @Test
     public void test() {
-        userXmlMapper.findAll().forEach(s -> System.out.println(s));
+        PageHelper.startPage(1, 4);
+        List<User> list = userXmlMapper.findAll();
+        PageInfo<User> info = new PageInfo<>(list);
+        System.out.println(info);
     }
 
 }
